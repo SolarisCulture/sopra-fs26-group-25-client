eba2a8ef0287ce864ea737d161ff6bdcb187d432
timmyho <timmyho2003@gmail.com>
2026-03-28 15:27:50 +0100

    Reapply "Merge branch 'task/59-implement-leave-lobby-button-that-triggers-reassignment-of-the-host-role-if-the-host-leaves' of https://github.com/SolarisCulture/sopra-fs26-group-25-client"
    This reverts commit 6719040f0a2d13a6eaa9e2891880656e3136d6f2.

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Student XX-XXX-XXX",
+  title: "Codenames-Group25",
   description: "sopra-fs26-template-client",
 };
 
@@ -43,7 +43,7 @@ export default function RootLayout({
             // if a component type needs special styling, setting here will override default options set in token
             components: {
               Button: {
-                colorPrimary: "#75bd9d", // this will color all buttons in #75bd9d, overriding the default primaryColor #22426b set in token line 35
+                colorPrimary: "	#A02A6C", // this will color all buttons in #75bd9d, overriding the default primaryColor #22426b set in token line 35
                 algorithm: true, // enable algorithm (redundant with line 33 but here for demo purposes)
                 controlHeight: 38,
               },
