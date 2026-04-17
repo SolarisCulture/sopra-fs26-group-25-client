6d44a0803236c2ecfcc8c8edb659df378bbbd5aa
Aldin <kiirusou@gmail.com>
2026-04-17 17:49:53 +0200

    Fix: removed the "/" from the link since it adds a / too much

--- app/utils/domain.ts
+++ app/utils/domain.ts
@@ -7,7 +7,7 @@ import { isProduction } from "@/utils/environment";
  */
 export function getApiDomain(): string {
   const prodUrl = process.env.NEXT_PUBLIC_PROD_API_URL ||
-    "https://sopra-fs26-group-25-server.oa.r.appspot.com/";
+    "https://sopra-fs26-group-25-server.oa.r.appspot.com";
   const devUrl = "http://localhost:8080";
   return isProduction() ? prodUrl : devUrl;
 }
